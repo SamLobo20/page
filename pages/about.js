bacab0a06d5662d16ab9cc70cff272694bc0def5
SamLobo20 <lobo.sam.69@gmail.com>
2021-10-29 13:43:15 -0500

    Create product page. Main structure.

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+const About = () => {
+
+    const x = 18;
+
+    return (
+        <main className="w-screen">
+            <div className="flex flex-col items-center justify-content text-center">
+                <h1 className="text-4xl mb-8">About us</h1>
+                <p className="shadow p-8 bg-gray-100 rounded-xl">{`sdcjdscjkdbcds csjcdsckbdscjbcjksdc ${x} dchjcdscdscj`}</p>
+            </div>
+        </main>
+    )
+
+
+}
+
+export default About
---
+++ pages/products.js
@@ -0,0 +1,54 @@
+import Link from 'next/link'
+
+const Products = () => {
+
+    const products = [
+        {
+            name: "sdkcbccsc",
+            price: 34.12
+        },
+        {
+            name: "xaskxjxasx",
+            price: 14.12
+        },
+        {
+            name: "hnhngn",
+            price: 3.12
+        },
+        {
+            name: "777777",
+            price: 344.12
+        },
+        {
+            name: "rrrrrrr",
+            price: 34768276.12
+        },
+
+    ]
+
+
+    return (
+        <main className="w-screen">
+            <div className="flex flex-col items-center justify-content text-center">
+                <h1 className="text-4xl mb-8">About us</h1>
+
+                {products.map((product) => {
+                    return (
+                        <Link href="htttp://www.google.com" key={product.name}>
+                            <a className="shadow p-8 bg-pink-600 rounded-xl mb-6 hover:text-white">
+                                <p className="csc">{`${product.name}`}</p>
+                                <p className="dcsdc">{`${product.price}`}</p>
+                            </a>
+                        </Link>
+                    )
+
+                })}
+
+            </div>
+        </main>
+    )
+
+
+}
+
+export default Products
