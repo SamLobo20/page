5e18752f303b6a1ef8ff6d14167df52a05c0cd6f
Christof <bauer.christof@gmail.com>
2021-11-05 12:12:55 -0500

    Justify content

--- pages/video.js
+++ pages/video.js
@@ -15,7 +15,7 @@ const Video = () => {
       <main className="w-screen pb-32">
         <h1 className="text-4xl my-8 text-center w-full">Video</h1>
 
-        <div className="flex flex-wrap gap-6 items-center mt-8">
+        <div className="flex flex-wrap gap-6 items-center justify-center mt-8">
           <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/MCbpgZSL5LM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
           <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/bk6Xst6euQk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
         </div>
