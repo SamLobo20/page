25d7e56a4c1907015081ace11029d002770b21ad
SamLobo20 <lobo.sam.69@gmail.com>
2021-11-08 12:31:32 -0500

    Update video.js

--- pages/video.js
+++ pages/video.js
@@ -1,7 +1,15 @@
 import Head from 'next/head'
+import { useState } from 'react';
 import Nav from '../components/Nav.js'
 
 const Video = () => {
+
+  const [isOpen, setIsOpen] = useState(true)
+
+  function openForm() {
+    setIsOpen(!isOpen)
+    const x = 1;
+  }
 
   return (
     <div>
@@ -12,15 +20,18 @@ const Video = () => {
 
       <Nav />
 
-      <main className="w-screen pb-32">
-        <h1 className="text-4xl my-8 text-center w-full">Video</h1>
+      <main className="w-screen pb-32 items-center justify-center">
+        <div className="flex flex-column justify-center">
+          <button onClick={openForm} className="items-center justify-center border-1 bg-brand px-8 py-4 w-48 text-white rounded shadow-2xl hover:shadow transition-all my-8">Video</button>
+        </div>
+      </main>
 
-        <div className="flex flex-wrap gap-6 items-center justify-center mt-8">
+      {isOpen &&
+        <div className="flex flex-wrap gap-6 items-center justify-center mt-8 ">
           <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/MCbpgZSL5LM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
           <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/bk6Xst6euQk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={true}></iframe>
         </div>
-
-      </main>
+      }
     </div>
   )
 }
