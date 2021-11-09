22f4618d8260cc1b2c65e577ae82ad08c0464c2e
Christof <bauer.christof@gmail.com>
2021-11-09 11:52:04 -0500

    Give Nav an active state

--- components/Nav.js
+++ components/Nav.js
@@ -1,18 +1,27 @@
+import { useRouter } from 'next/dist/client/router'
 import Link from 'next/link'
 
 const Nav = () => {
 
+    const router = useRouter()
     const links = [
-        { name: "Home", link: "/" },
-        { name: "Posts", link: "/posts" },
-        { name: "Contact", link: "/contact" },
-        { name: "Video", link: "/video" },
+        { name: "Home", url: "/" },
+        { name: "Posts", url: "/posts" },
+        { name: "Contact", url: "/contact" },
+        { name: "Video", url: "/video" },
     ]
+
     return (
-        <ul className="flex space-x-6 my-4 ml-4">
+        <ul className="flex space-x-6 my-4 ml-4 mb-12">
             {links.map((l) => {
                 return (
-                    <li key={l.name}><Link href={l.link}><a>{l.name}</a></Link></li>
+                    <li key={l.name} className='w-16'>
+                        <Link href={l.url}>
+                            <a className={`${router.pathname === l.url ? 'active-nav' : ''}`}>
+                                {l.name}
+                            </a>
+                        </Link>
+                    </li>
                 )
             })}
         </ul>
