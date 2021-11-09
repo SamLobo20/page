b6cd488b1423b83d09353ef6f4449dc1b191d39e
Christof <bauer.christof@gmail.com>
2021-11-09 12:15:23 -0500

    Add loading bar!

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import NextNprogress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="var(--color-brand)"
+        startPosition={0.3}
+        stopDelayMs={100}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
